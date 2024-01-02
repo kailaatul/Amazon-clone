0193b8967199349f531e69cb65344db42a6da9ff
Atul <atulkumar@gharnicompany.com>
2024-01-02 21:37:35 +0530

    ankit tell me JS

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
 //  logo redirecct to homepage
 
+var isopenche = false;
+
 function homepage() {
-    window.location.href="./index.html";
+    window.location.href = "./index.html";
 }
 
 //  all category option below
 
-function allCateShow () {
-    
+function allCateShow() {
+
 }
+
+console.log("This is fired")
+var allButton = document.getElementById("allbutton");
+
+allButton.onclick =  function(e){
+
+    if (isopenche == true) {
+        document.getElementById("all-category-drop-down").style.display = "none";
+        isopenche = false;
+    }else{
+        
+        document.getElementById("all-category-drop-down").style.display = "flex";
+        isopenche = true;
+    }
+    e.preventDefault();
+    e.stopPropagation();
+    
+};
+
+console.log(allButton);
+
+var locationbutton = document.getElementById("locationbutton");
+locationbutton.onclick = locationniclick;
+
+function locationniclick(e){
+    var popup = document.getElementById("popupforlocation");
+    popup.style.display = "flex";
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+document.onclick = function(){
+    var popup = document.getElementById("popupforlocation");
+    popup.style.display = "none";
+
+    document.getElementById("all-category-drop-down").style.display = "none";
+
+}
+
+// function havebadhuloadthayjiyu(){
+//     //console.log("Badhu j load thay jiyu");
+
+    
+
+// }
+
+// document.addEventListener("DOMContentLoaded", havebadhuloadthayjiyu);
+
+
